4d19782979bfd65ea7b4c9718df602775df0153f
Daniel Gonzalez Nothnagel <daniel.gonzalez.nothnagel@sap.com>
2026-06-02 12:42:36 +0200

    Implement `HasLabels` and `MatchingLabels` ListOptions
    These ListOptions allow to filter objects based on their labels.
    
    Signed-off-by: Daniel Gonzalez Nothnagel <daniel.gonzalez.nothnagel@sap.com>

--- storeutils/store/listoptions.go
+++ storeutils/store/listoptions.go
@@ -5,6 +5,7 @@ package store
 
 import (
 	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/selection"
 )
 
 type ListOptions struct {
@@ -13,4 +14,36 @@ type ListOptions struct {
 
 type ListOption interface {
 	ApplyToList(*ListOptions)
+}
+
+type MatchingLabels labels.Set
+
+func (m MatchingLabels) ApplyToList(o *ListOptions) {
+	sel := labels.SelectorFromSet(labels.Set(m))
+	o.LabelSelector = andSelectors(o.LabelSelector, sel)
+}
+
+type HasLabels []string
+
+func (h HasLabels) ApplyToList(o *ListOptions) {
+	sel := labels.NewSelector()
+	for _, key := range h {
+		req, err := labels.NewRequirement(key, selection.Exists, nil)
+		// Invalid labels are ignored, leading to an empty selector.
+		// This is in line with the behavior of controller-runtime:
+		// https://github.com/kubernetes-sigs/controller-runtime/blob/main/pkg/client/options.go#L637-L644
+		if err != nil {
+			continue
+		}
+		sel = sel.Add(*req)
+	}
+	o.LabelSelector = andSelectors(o.LabelSelector, sel)
+}
+
+func andSelectors(existing, additional labels.Selector) labels.Selector {
+	if existing == nil {
+		return additional
+	}
+	reqs, _ := additional.Requirements()
+	return existing.Add(reqs...)
 }
