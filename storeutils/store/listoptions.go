741b4a701bb3b74d8d5627ed48e8efe1b645baed
Daniel Gonzalez Nothnagel <daniel.gonzalez.nothnagel@sap.com>
2026-06-02 12:42:36 +0200

    Implement `HasLabels` and `MatchingLabels` ListOptions
    These ListOptions allow to filter objects based on their labels.
    
    Signed-off-by: Daniel Gonzalez Nothnagel <daniel.gonzalez.nothnagel@sap.com>

--- storeutils/store/listoptions.go
+++ storeutils/store/listoptions.go
@@ -5,6 +5,7 @@ package store
 
 import (
 	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/selection"
 )
 
 type ListOptions struct {
@@ -13,4 +14,30 @@ type ListOptions struct {
 
 type ListOption interface {
 	ApplyToList(*ListOptions)
+}
+
+type MatchingLabels map[string]string
+
+func (m MatchingLabels) ApplyToList(o *ListOptions) {
+	sel := labels.SelectorFromSet(labels.Set(m))
+	o.LabelSelector = andSelectors(o.LabelSelector, sel)
+}
+
+type HasLabels []string
+
+func (h HasLabels) ApplyToList(o *ListOptions) {
+	sel := labels.NewSelector()
+	for _, key := range h {
+		req, _ := labels.NewRequirement(key, selection.Exists, nil)
+		sel = sel.Add(*req)
+	}
+	o.LabelSelector = andSelectors(o.LabelSelector, sel)
+}
+
+func andSelectors(existing, additional labels.Selector) labels.Selector {
+	if existing == nil {
+		return additional
+	}
+	reqs, _ := additional.Requirements()
+	return existing.Add(reqs...)
 }
